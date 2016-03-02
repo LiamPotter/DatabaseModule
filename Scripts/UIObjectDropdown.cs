11e25a0df8bf2c3c57ad3ad0f692f7b6e0fc5591
acowling <cowlinga@optusnet.com.au>
2016-03-02 14:58:56 +1000

    Added in some more UI code elements

---
+++ Scripts/UIObjectDropdown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIObjectDropdown : MonoBehaviour {
+    public RectTransform infoContainer;
+    public bool isOpen;
+    void Start()
+    {
+        infoContainer = transform.FindChild("InfoContainer").GetComponent<RectTransform>();
+        isOpen = false;
+    }
+    void Update()
+    {
+        Vector3 scale = infoContainer.localScale;
+        scale.y = Mathf.Lerp(scale.y,isOpen ? 1:0, Time.deltaTime * 20);
+        infoContainer.localScale = scale;
+    }
+    public void OnButtonPress()
+    {
+        isOpen = !isOpen;
+        Debug.Log("trying to open");
+    }
+}
